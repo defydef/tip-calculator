80c4433db93a526b50bf7e8ba54d763e33ab3cf5
defy <8841170+defydef@users.noreply.github.com>
2023-07-24 08:11:46 +0700

    Add ReusableInput component Use ReusableInput component to enter tip and bill amount.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [myTip, setMyTip] = useState(0);
+  const [friendTip, setFriendTip] = useState(0);
+
+  function handleAmount(amount, inputType) {
+    if (inputType === "bill") {
+      setBill(Number(amount));
+    } else if (inputType === "myTip") {
+      setMyTip(Number(amount));
+    } else {
+      setFriendTip(Number(amount));
+    }
+  }
+
+  let totalTip = (myTip / 100 + friendTip / 100) * bill;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReusableInput amount={bill} onInput={handleAmount} inputType="bill" />
+      <ReusableInput amount={myTip} onInput={handleAmount} inputType="myTip" />
+      <ReusableInput
+        amount={friendTip}
+        onInput={handleAmount}
+        inputType="friendTip"
+      />
+      <h3>
+        You pay ${totalTip + bill} (${bill} + ${totalTip} tip)
+      </h3>
+      <button>Reset</button>
+    </div>
+  );
+}
+
+function ReusableInput({ bill, onInput, inputType }) {
+  return (
+    <div>
+      <p>
+        How much was the bill? &nbsp;
+        <input
+          type="text"
+          value={bill}
+          onChange={(e) => onInput(e.target.value, inputType)}
+        />
+      </p>
     </div>
   );
 }
